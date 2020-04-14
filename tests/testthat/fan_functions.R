0adb48424bc24ec360beba6b6807e8ba0c1572a1
Evan Mavrin <emavrin1@binghamton.edu>
2020-04-13 23:38:32 -0400

    Fix to tests

--- tests/testthat/fan_functions.R
+++ tests/testthat/fan_functions.R
@@ -4,7 +4,7 @@ library(maps)
 
 datapath <- system.file("extdata", "response.json", package = "mypackage")
 test_that("Functions are returning correct objects", {
-  is_a(fars_read(accident_2015), tbl_df)
+  is_a(fars_read("accident_2015"), tbl_df)
   is_a(make_filename(2015), string)
   is_a(fars_read_years(c(2014, 2015)), tbl_df)
   is_a(fars_summarize_years(2015), tbl_df)
