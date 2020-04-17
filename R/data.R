065f9ff6ff801cdf57cd86cde6654e32d580d65d
Evan Mavrin <emavrin1@binghamton.edu>
2020-04-16 23:48:50 -0400

    Documentation for data, and a fix to the vignette file

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Fatal injuries suffered in motor vehicle traffic crashes in the US, 2013
+#'
+#' @source NHTSA - Fatality Analysis Reporting System (FARS) \url{https://www.nhtsa.gov/node/97996/251}
+
+#' @examples
+#' \dontrun{
+#'  accident_2013
+#' }
+"accident_2013"
+
+#' Fatal injuries suffered in motor vehicle traffic crashes in the US, 2014
+#'
+#' @source NHTSA - Fatality Analysis Reporting System (FARS) \url{https://www.nhtsa.gov/node/97996/251}
+
+#' @examples
+#' \dontrun{
+#'  accident_2013
+#' }
+"accident_2014"
+
+#' Fatal injuries suffered in motor vehicle traffic crashes in the US, 2015
+#'
+#' @source NHTSA - Fatality Analysis Reporting System (FARS) \url{https://www.nhtsa.gov/node/97996/251}
+
+#' @examples
+#' \dontrun{
+#'  accident_2013
+#' }
+"accident_2015"
